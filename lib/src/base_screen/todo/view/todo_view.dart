7fb0048790c40ea61022d3269e693b6608adc3eb
younasud <younasjax@gmail.com>
2022-01-07 22:24:17 +0500

    colors define

--- lib/src/base_screen/todo/view/todo_view.dart
+++ lib/src/base_screen/todo/view/todo_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:todo/resources/resources.dart';
 import 'package:todo/src/base_screen/todo/view/widgets/create_todo.dart';
 import 'package:todo/src/base_screen/todo/view/widgets/custom_dialog.dart';
 import 'package:todo/src/base_screen/todo/view/widgets/tasks_list.dart';
@@ -15,11 +16,6 @@ class TodoView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leadingWidth: 80,
-
-        // title: Text(
-        //   "aman",
-        //   style: TextStyle(color: Colors.black),
-        // ),
         backgroundColor: Colors.transparent,
         elevation: 0.0,
       ),
@@ -28,7 +24,7 @@ class TodoView extends StatelessWidget {
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-            UserAccountsDrawerHeader(
+            const UserAccountsDrawerHeader(
               decoration: BoxDecoration(
                 color: Colors.deepOrange,
               ),
@@ -49,8 +45,8 @@ class TodoView extends StatelessWidget {
                   )),
             ),
             ListTile(
-              leading: Icon(Icons.check_box),
-              title: Text('Check All'),
+              leading: const Icon(Icons.check_box),
+              title: const Text('Check All'),
               enabled: true,
               onTap: () {
                 checkAll(context);
@@ -60,8 +56,8 @@ class TodoView extends StatelessWidget {
               onTap: () {
                 uncheckAll(context);
               },
-              leading: Icon(Icons.check_box_outline_blank_rounded),
-              title: Text('UnCheck All'),
+              leading: const Icon(Icons.check_box_outline_blank_rounded),
+              title: const Text('UnCheck All'),
             ),
             ListTile(
               onTap: () {
@@ -73,22 +69,22 @@ class TodoView extends StatelessWidget {
                       'This is irreversible, you might end up losing all your tasks listed here.\n Are you sure ?',
                   buttons: [
                     DialogButton(
-                      child: Text(
+                      child: const Text(
                         "NO",
                         style: TextStyle(color: Colors.white, fontSize: 20),
                       ),
                       onPressed: () => Navigator.pop(context),
-                      color: Color.fromRGBO(0, 179, 134, 1.0),
+                      color: const Color.fromRGBO(0, 179, 134, 1.0),
                     ),
                     DialogButton(
-                      child: Text(
+                      child: const Text(
                         "YES",
                         style: TextStyle(color: Colors.white, fontSize: 20),
                       ),
                       onPressed: () {
                         deleteAll(context);
                       },
-                      gradient: LinearGradient(colors: [
+                      gradient: const LinearGradient(colors: [
                         Color.fromRGBO(116, 116, 191, 1.0),
                         Color.fromRGBO(52, 138, 199, 1.0)
                       ]),
@@ -96,10 +92,10 @@ class TodoView extends StatelessWidget {
                   ],
                 ).show();
               },
-              leading: Icon(Icons.delete),
-              title: Text('Delete All'),
+              leading: const Icon(Icons.delete),
+              title: const Text('Delete All'),
             ),
-            ListTile(
+            const ListTile(
               onTap: null,
               leading: Icon(Icons.color_lens_sharp),
               title: Text('Change Theme'),
@@ -117,13 +113,13 @@ class TodoView extends StatelessWidget {
                       );
                     });
               },
-              leading: Icon(Icons.info),
-              title: Text('About'),
+              leading: const Icon(Icons.info),
+              title: const Text('About'),
             ),
           ],
         ),
       ),
-      backgroundColor: Colors.deepOrange,
+      backgroundColor: R.colors.themeColor,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           print('button pressed');
@@ -131,29 +127,29 @@ class TodoView extends StatelessWidget {
               context: context, builder: (context) => CreateTodoView());
         },
         elevation: 12.0,
-        backgroundColor: Colors.deepOrange,
-        child: Icon(Icons.add),
+        backgroundColor: R.colors.themeColor,
+        child: const Icon(Icons.add),
       ),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              padding:
-                  EdgeInsets.only(top: 0.0, left: 30, right: 30.0, bottom: 5.0),
+              padding: const EdgeInsets.only(
+                  top: 0.0, left: 30, right: 30.0, bottom: 5.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Column(
                     children: [
-                      Image(
+                      const Image(
                         height: 100,
                         width: 100,
                         image: AssetImage(
                           "assets/logo_transparent.png",
                         ),
                       ),
-                      Text(
+                      const Text(
                         "To Do",
                         style: TextStyle(
                           fontSize: 50.0,
@@ -169,7 +165,7 @@ class TodoView extends StatelessWidget {
                       Container(
                         decoration: BoxDecoration(
                           color: Colors.lightBlueAccent,
-                          borderRadius: BorderRadius.only(
+                          borderRadius: const BorderRadius.only(
                               topLeft: Radius.circular(10),
                               topRight: Radius.circular(10),
                               bottomLeft: Radius.circular(10),
@@ -179,22 +175,22 @@ class TodoView extends StatelessWidget {
                               color: Colors.grey.withOpacity(0.5),
                               spreadRadius: 5,
                               blurRadius: 7,
-                              offset:
-                                  Offset(0, 3), // changes position of shadow
+                              offset: const Offset(
+                                  0, 3), // changes position of shadow
                             ),
                           ],
                         ),
-                        padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
-                        margin: EdgeInsets.all(10),
+                        padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                        margin: const EdgeInsets.all(10),
                         child: Text(
                           '${Provider.of<TodoVM>(context).tasks.length}',
-                          style: TextStyle(
+                          style: const TextStyle(
                               fontSize: 60.0,
                               color: Colors.white,
                               fontWeight: FontWeight.w900),
                         ),
                       ),
-                      Text(
+                      const Text(
                         ' Tasks left',
                         style: TextStyle(
                           fontSize: 20.0,
@@ -210,11 +206,11 @@ class TodoView extends StatelessWidget {
             ),
             Expanded(
               child: Container(
-                padding: EdgeInsets.symmetric(
+                padding: const EdgeInsets.symmetric(
                   horizontal: 2.0,
                   vertical: 2.0,
                 ),
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(20.0),
